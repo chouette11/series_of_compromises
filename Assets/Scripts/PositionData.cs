15386db889ec8749e2584c689cf4470afda02aff
chouette11 <keifukuda1115@gmail.com>
2024-03-20 03:17:29 +0900

    Fix position data

--- Assets/Scripts/PositionData.cs
+++ Assets/Scripts/PositionData.cs
@@ -1,7 +1,11 @@
 [System.Serializable]
 public class PositionData
 {
+    public string id;
     public float x;
     public float y;
     public float z;
+    public string typeText;
+    public bool isVisible;
+    public string sender;
 }
