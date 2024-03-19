370b7f71584b903175976ba6e191b0e69ea2b1bf
yu371 <yuyu14191567@gmail.com>
2024-03-19 16:22:14 +0900

    HPbae_change

--- Assets/Scripts/UsePositionData.cs
+++ Assets/Scripts/UsePositionData.cs
@@ -5,6 +5,7 @@ public class UsePositionData : MonoBehaviour
 {
     public GameObject Enemyobject;
     public WebSocketClient webSocketClient;
+    private GameObject enemy;
 
     void Update()
     {
@@ -14,7 +15,7 @@ public class UsePositionData : MonoBehaviour
 
             // 例えば、受け取った位置データを何かのGameObjectの位置として使用する
             Vector3 newPosition = new Vector3(webSocketClient.LatestPosition.x * 16, webSocketClient.LatestPosition.y, 20 - webSocketClient.LatestPosition.z * 20);
-            Instantiate(Enemyobject,newPosition,Quaternion.identity);
+            Instantiate(Enemyobject,newPosition, enemy.transform.rotation);
             //transform.position = newPosition;
             webSocketClient.LatestPosition = null;
 
