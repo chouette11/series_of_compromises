8c8ed233852eff0e0ed25d1add4a579dcc1cd6d5
yu371 <yuyu14191567@gmail.com>
2024-03-20 16:53:09 +0900

    last_commit

--- Assets/Scripts/weaponchanger.cs
+++ Assets/Scripts/weaponchanger.cs
@@ -7,48 +7,63 @@ public class weaponchanger : MonoBehaviour
 {
 
 
-    public GameObject weapon; // 現在持っているオブジェクト
-    public GameObject weapon1;
-    public GameObject weapon2; // 入れ替える新しいオブジェクトのプレハブ
-    private WebSocketClient webSocketClient;
+  public GameObject weapon; // 現在持っているオブジェクト
+  public GameObject weapon1;
+  public GameObject weapon2; // 入れ替える新しいオブジェクトのプレハブ
+  private WebSocketClient webSocketClient;
 
 
   private GameObject currentObject;
-    // オブジェクトを入れ替えるメソッド
-    void Start() {
-        webSocketClient = GameObject.Find("WebsocketFloor").GetComponent<WebSocketClient>();
-    }
-    public void SwapObject()
+
+  private int weaponeNumber = 1;
+  // オブジェクトを入れ替えるメソッド
+  void Start()
+  {
+    webSocketClient = GameObject.Find("WebsocketFloor").GetComponent<WebSocketClient>();
+  }
+  public void SwapObject()
+  {
+    // if (currentObject != null)
+    // {
+    //     Destroy(currentObject); // 現在のオブジェクトを削除
+    // }
+
+    // 新しいオブジェクトを手の位置にインスタンス化
+
+  }
+  void Update()
+  {
+    if (webSocketClient.weaponNumber != 4 && weaponeNumber != webSocketClient.weaponNumber)
     {
-        // if (currentObject != null)
-        // {
-        //     Destroy(currentObject); // 現在のオブジェクトを削除
-        // }
+      if (webSocketClient.weaponNumber == 0)
+      {
+        Destroy(currentObject);
+        currentObject = Instantiate(weapon, transform.position, transform.rotation, transform);
+        weaponeNumber = webSocketClient.weaponNumber;
 
-        // 新しいオブジェクトを手の位置にインスタンス化
+        webSocketClient.weaponNumber = 4;
 
-    }
-    void Update (){    
-        if(webSocketClient.weaponNumber !=4){
-            if (webSocketClient.weaponNumber == 0){
-              Destroy(currentObject);  
-              currentObject = Instantiate(weapon, transform.position, transform.rotation, transform);
-            webSocketClient.weaponNumber = 4;
-            }
-            else if (webSocketClient.weaponNumber == 1){
-                 Destroy(currentObject);  
-              currentObject = Instantiate(weapon1, transform.position, transform.rotation, transform);
-            webSocketClient.weaponNumber = 4;
-            }
-            else if (webSocketClient.weaponNumber == 2){
-             Destroy(currentObject);  
-              currentObject = Instantiate(weapon2, transform.position, transform.rotation, transform);
-            webSocketClient.weaponNumber = 4;
-            }
+      }
+      else if (webSocketClient.weaponNumber == 1)
+      {
+        Destroy(currentObject);
+        currentObject = Instantiate(weapon1, transform.position, transform.rotation, transform);
+        weaponeNumber = webSocketClient.weaponNumber;
 
-        }
+        webSocketClient.weaponNumber = 4;
+      }
+      else if (webSocketClient.weaponNumber == 2)
+      {
+        Destroy(currentObject);
+        currentObject = Instantiate(weapon2, transform.position, transform.rotation, transform);
+        weaponeNumber = webSocketClient.weaponNumber;
+
+        webSocketClient.weaponNumber = 4;
+      }
 
     }
+
+  }
 
 
 }
