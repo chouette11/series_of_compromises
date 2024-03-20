5d0207978e24913287977204e6d6e12b3875baa7
chouette11 <keifukuda1115@gmail.com>
2024-03-20 12:53:56 +0900

    Fix tower

--- Assets/Scripts/towescript.cs
+++ Assets/Scripts/towescript.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class towescript : MonoBehaviour
 {
     public bool Hpmanage = true;
     public int towerHp = 50;
     public GameObject Hpslide;
     private Slider hpslidscript;
+    private WebSocketClient webSocketClient;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,7 @@ public class towescript : MonoBehaviour
         towerHp = 50;
         hpslidscript = Hpslide.GetComponent<Slider>();
         hpslidscript.value = 1;
+        webSocketClient = GameObject.Find("WebsocketFloor").GetComponent<WebSocketClient>();
     }
 
     // Update is called once per frame
@@ -26,6 +31,9 @@ public class towescript : MonoBehaviour
             towerHp = towerHp - 1;
             Hpmanage = true;
             hpslidscript.value = (float)towerHp / 50;
+            SendPositionData sendPositionData = new SendPositionData(0, 0, 0, towerHp.ToString(), "hp", true, "unity", "uid");
+            webSocketClient.SendMessageToServer(sendPositionData);
+
         }
     }
 }
